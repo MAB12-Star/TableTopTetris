e17a3e56946ce4df3c5f05abd4a487a740389a5a
Michael <michael.aron.blue@gmail.com>
2024-04-29 14:43:58 -0600

    SFX Updates

--- Assets/1_Tetris_Building_Blocks/Scripts/GridUnit.cs
+++ Assets/1_Tetris_Building_Blocks/Scripts/GridUnit.cs
@@ -17,7 +17,7 @@ public class GridUnit : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("cube_child"))
+        if (other.CompareTag("cube_child") || (other.CompareTag("child")))
         {
             
             IsOccupied = true;
@@ -26,30 +26,14 @@ public class GridUnit : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("cube_child"))
+        if (other.CompareTag("cube_child") || (other.CompareTag("child")))
         {
             
             IsOccupied = false;
         }
     }
 
-    // Perform a double check to confirm occupancy status
-    public void DoubleCheckOccupancy()
-    {
-        // Check if any box collider overlaps with this grid unit
-        Collider[] overlappingColliders = Physics.OverlapBox(transform.position, transform.localScale / 2f);
-        foreach (Collider collider in overlappingColliders)
-        {
-            if (collider.CompareTag("child") || (collider.CompareTag("cube_child")))
-            {
-                // Mark the grid unit as occupied
-                IsOccupied = true;
-                return; // Exit loop after finding one overlapping collider
-            }
-        }
-        // No overlapping collider found, mark the grid unit as vacant
-        IsOccupied = false;
-    }
+   
 
     public void finalLayerCheckOccupancy()
     {
