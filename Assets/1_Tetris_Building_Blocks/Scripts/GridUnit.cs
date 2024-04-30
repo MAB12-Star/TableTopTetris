f2146e91a2dfc0e69e4b54c5d8fec4d83e2b0012
Michael <michael.aron.blue@gmail.com>
2024-04-29 22:04:56 -0600

    update gridUnit
    update

--- Assets/1_Tetris_Building_Blocks/Scripts/GridUnit.cs
+++ Assets/1_Tetris_Building_Blocks/Scripts/GridUnit.cs
@@ -34,7 +34,23 @@ public class GridUnit : MonoBehaviour
         }
     }
 
-   
+    // Perform a double check to confirm occupancy status
+    public void DoubleCheckOccupancy()
+    {
+        // Check if any box collider overlaps with this grid unit
+        Collider[] overlappingColliders = Physics.OverlapBox(transform.position, transform.localScale / 2f);
+        foreach (Collider collider in overlappingColliders)
+        {
+            if (collider.CompareTag("child") || (collider.CompareTag("cube_child")))
+            {
+                // Mark the grid unit as occupied
+                IsOccupied = true;
+                return; // Exit loop after finding one overlapping collider
+            }
+        }
+        // No overlapping collider found, mark the grid unit as vacant
+        IsOccupied = false;
+    }
 
     public void finalLayerCheckOccupancy()
     {
